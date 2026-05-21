9ca610923f361d10bc2eabe79d9e98803da3475c
Adrian Valdes <adriangvaldes@gmail.com>
2026-05-20 23:37:52 -0300

    feat: add download functionality and integrate scroll area in upload widget

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -3,6 +3,7 @@ import * as Progress from '@radix-ui/react-progress'
 import { Download, ImageUp, Link2, RefreshCcw, X } from 'lucide-react'
 import { motion } from 'motion/react'
 import { type Upload, useUploads } from '../store/uploads'
+import { downloadUrl } from '../utils/download-url'
 import { formatBytes } from '../utils/format-bytes'
 import { Button } from './ui/button'
 
@@ -13,6 +14,7 @@ interface UploadWidgetUploadItemProps {
 
 export function UploadWidgetUploadItem({ upload, uploadId }: UploadWidgetUploadItemProps) {
   const cancelUpload = useUploads((store) => store.cancelUpload)
+  const retryUpload = useUploads((store) => store.retryUpload)
 
   const progress = Math.min(
     upload.compressedSizeInBytes
@@ -33,15 +35,24 @@ export function UploadWidgetUploadItem({ upload, uploadId }: UploadWidgetUploadI
       <div className="flex flex-col gap-1">
         <span className="text-xs font-medium flex items-center gap-1">
           <ImageUp className="size-3 text-zinc-300" strokeWidth={1.5} />
-          <span>{upload.name}</span>
+          <span className="max-w-[180px] truncate">{upload.name}</span>
         </span>
 
         <span className="text-xxs text-zinc-400 flex gap-1.5 items-center">
           <span className="line-through">{formatBytes(upload.originalSizeInBytes)}</span>
           <div className="size-1 rounded-full bg-zinc-700" />
           <span>
-            300KB
-            <span className="text-green-400 ml-1">-94%</span>
+            {formatBytes(upload.compressedSizeInBytes ?? 0)}
+            {upload.compressedSizeInBytes && (
+              <span className="text-green-400 ml-1">
+                -
+                {Math.round(
+                  ((upload.originalSizeInBytes - upload.compressedSizeInBytes) * 100) /
+                    upload.originalSizeInBytes,
+                )}
+                %
+              </span>
+            )}
           </span>
           <div className="size-1 rounded-full bg-zinc-700" />
 
@@ -65,8 +76,16 @@ export function UploadWidgetUploadItem({ upload, uploadId }: UploadWidgetUploadI
         />
       </Progress.Root>
 
-      <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
-        <Button size="icon-sm" disabled={upload.status !== 'success'}>
+      <div className="absolute top-2 right-2 flex items-center gap-1">
+        <Button
+          size="icon-sm"
+          aria-disabled={!upload.remoteUrl}
+          onClick={() => {
+            if (upload.remoteUrl) {
+              downloadUrl(upload.remoteUrl)
+            }
+          }}
+        >
           <Download className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Download compressed image</span>
         </Button>
@@ -80,7 +99,11 @@ export function UploadWidgetUploadItem({ upload, uploadId }: UploadWidgetUploadI
           <span className="sr-only">Copy remote URL</span>
         </Button>
 
-        <Button disabled={!['canceled', 'error'].includes(upload.status)} size="icon-sm">
+        <Button
+          disabled={!['canceled', 'error'].includes(upload.status)}
+          size="icon-sm"
+          onClick={() => retryUpload(uploadId)}
+        >
           <RefreshCcw className="size-4" strokeWidth={1.5} />
           <span className="sr-only">Retry upload</span>
         </Button>
