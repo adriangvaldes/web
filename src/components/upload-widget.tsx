ff117d2bb922417b96eafa1c1d699777465ea710
Adrian Valdes <adriangvaldes@gmail.com>
2026-05-20 22:28:34 -0300

    feat: integrate pending uploads tracking with global progress percentage

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,33 +1,20 @@
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { motion, useCycle } from 'motion/react'
+import { usePendingUploads } from '../store/uploads'
 import { UploadWidgetDropzone } from './upload-widget-dropzone'
 import { UploadWidgetHeader } from './upload-widget-header'
 import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button'
 import { UploadWidgetUploadList } from './upload-widget-upload-list'
 
-const uploadWidgetClassName = [
-  'bg-zinc-900',
-  'overflow-hidden',
-  'w-[360px]',
-  'rounded-xl',
-  'border',
-  'border-transparent',
-  'animate-border',
-  'data-[state=open]:shadow-shape',
-  'data-[state=closed]:rounded-3xl',
-  'data-[state=closed]:data-[progress=false]:shadow-shape',
-  'data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,var(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),color-mix(in_oklab,var(--color-zinc-700)_48%,transparent)_80%,var(--color-indigo-500)_86%,var(--color-indigo-300)_90%,var(--color-indigo-500)_94%,color-mix(in_oklab,var(--color-zinc-600)_48%,transparent))_border-box]',
-].join(' ')
-
 export function UploadWidget() {
-  const isThereAnyPendingUploads = true
+  const { isThereAnyPendingUploads } = usePendingUploads()
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true)
 
   return (
     <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
       <motion.div
         data-progress={isThereAnyPendingUploads}
-        className={uploadWidgetClassName}
+        className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape  data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
         animate={isWidgetOpen ? 'open' : 'closed'}
         variants={{
           closed: {
